71f4fe06d9de2a036842c92ae0b64bda86c05784
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:23:24 -0300

    evitar cadastro de mais de um aluno com o mesmo cpf

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -16,8 +16,11 @@ export class AppComponent {
    title = "ta-gui";
 
    gravar(a: Aluno): void {
-     this.alunoService.gravar(a);
-     this.aluno = {nome: "", cpf: "", email: "", login: ""};
-     this.alunos.push(a);
+     if (this.alunoService.gravar(a)) {
+       this.alunos.push(a);
+       this.aluno = {nome: "", cpf: "", email: "", login: ""};
+     } else {
+       this.aluno.cpf = "";
+     }
   }
 }
